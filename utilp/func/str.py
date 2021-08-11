82b89267d2ade46d273bb95540d45d68e6c2a67a
Shay Palachy <shaypal5@gmail.com>
2021-08-11 16:22:04 +0300

    str functions

---
+++ utilp/func/str.py
@@ -0,0 +1,29 @@
+"""String utility functions for Python 3."""
+
+import re
+
+
+def str_replace_by_dict(string: str, rep_dict: dict) -> str:
+    """Replaces multiple source-destination substrings by a given dict.
+
+    Parameters
+    ----------
+    string : str
+        The string to replace substrings in.
+    rep_dict : dict
+        A dictionary of source-destination mappings to use for replacement.
+
+    Returns
+    -------
+    str
+        The resulting string after substring replacements.
+
+    Example
+    -------
+        >>> rep_dict = {'cat': 'dog', 'a': 'b'}
+        >>> str_replace_by_dict('my cat is a friend', rep_dict)
+        "my dog is b friend"
+    """
+    rep = dict((re.escape(k), v) for k, v in rep_dict.items())
+    pattern = re.compile("|".join(rep.keys()))
+    return pattern.sub(lambda m: rep[re.escape(m.group(0))], string)
